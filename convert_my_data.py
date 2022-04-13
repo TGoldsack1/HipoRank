5bae078413536c76bbda484e034cf7edebb9cd7d
tgoldsack1 <tgoldsack1@sheffield.ac.uk>
2022-04-13 09:09:07 +0100

    changed convert file

--- convert_my_data.py
+++ convert_my_data.py
@@ -1,6 +1,6 @@
 '''
 Convert my data into expected format:
-.txt file, with each line in the format {"article_id": str, "article_text": [str]}
+.txt file, with each line in the format {"article_id": str, "article_text": [str], "abstract_txt": [str]}
 '''
 
 import json
@@ -12,20 +12,14 @@ for fp in data_fps:
     data = json.loads(in_file.read())
 
   out_docs = open(fp.replace(".json", ".txt"), "w")
-  out_sums = open(fp.replace(".json", "_gs.txt"), "w")
 
   for inst_dict in data:
     doc = inst_dict['abstract'] +  [item for sublist in inst_dict['sections'] for item in sublist]
     summ = inst_dict['summary']
 
-    out_docs.write(json.dumps({"article_id": inst_dict['id'], "article_text": doc}))
+    out_docs.write(json.dumps({"article_id": inst_dict['id'], "article_text": doc, "abstract_text": summ}))
     out_docs.write("\n")
 
-    out_sums.write(json.dumps({"article_id": inst_dict['id'], "summary_text": summ}))
-    out_sums.write("\n")
-
   out_docs.close()
-  out_sums.close()
-
 
     
